54fad25cb5f69a9e65648ae30ec0a3e260cf36bf
Matteo Gambino <62847924+matteo6198@users.noreply.github.com>
2022-01-05 22:06:13 +0100

    fix

--- src/Utils/util.py
+++ src/Utils/util.py
@@ -7,14 +7,14 @@ def save_checkpoint(args, state, is_best, filename):
     model_path = join(args.output_folder, filename)
     torch.save(state, model_path)
     if is_best:
-        shutil.copyfile(model_path, join(args.output_folder, "best_model.pth"))
+        shutil.copyfile(model_path, join(args.output_folder, "/best_model.pth"))
 
 def resume_train(path, model, optimizer):
     ''' 
     resume the training from the last epoch reached
     returns: last_epoch_reached, best_r5, not_improved_num
     '''
-    obj = torch.load(path + 'last_model.pth')
+    obj = torch.load(path + '/last_model.pth')
     model.load_state_dict(obj['model_state_dict'])
     optimizer.load_state_dict(obj['optimizer_state_dict'])
     epoch = obj['epoch_num'] + 1
