18f417701e0a216920a1f820b847d6a43c3236ab
Matteo Gambino <matteo6198@live.it>
2022-01-26 09:28:32 +0000

    added resize all dataset

---
+++ src/Utils/resize_all_img.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import torch
+import faiss
+import logging
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+from PIL import Image
+from os.path import join
+import torchvision.transforms as t
+
+TRANSFORMS = {
+    '1.5x': t.Resize((720, 960), interpolation=3),
+    '0.5x': t.Resize((240, 320), interpolation=3)
+}
+
+DATASETS_FOLDERS =[
+    '/content/pitts30k',
+    '/content/st_lucia'
+] 
+
+def path_to_pil_img(path):
+    return Image.open(path).convert("RGB")
+
+def resize_dataset(path, transforms):
+    paths = glob(join(path, "**", "*.jpg"), recursive=True)
+    for p in paths:
+        print(p)
+        img = path_to_pil_img(p)
+        t_img = TRANSFORMS[transforms](img)
+        t_img.save(p)
+
+if __name__ == '__main__':
+    try:
+        t_str = sys.argv[1]
+        if not(t_str in TRANSFORMS):
+            print(f'Unknown transform: possible values are ({TRANSFORMS})')
+    except:
+        print('Error: usage python3 resize_all_img.py <transform_name>')
+        exit()
+    
+    for p in DATASETS_FOLDERS:
+        resize_dataset(p, t_str)
