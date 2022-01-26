336523ea7a8fe057edd458a05774792d60e94e83
Matteo Gambino <matteo6198@live.it>
2022-01-26 08:31:21 +0000

    fix

--- src/train.py
+++ src/train.py
@@ -45,6 +45,7 @@ else:
 
 commons.setup_logging(args.output_folder)
 commons.make_deterministic(args.seed)
+torch.multiprocessing.set_start_method('spawn')
 
 if not(os.path.isfile(join(args.output_folder, 'args.pth'))):    
     torch.save(args, join(args.output_folder, 'args.pth'))
