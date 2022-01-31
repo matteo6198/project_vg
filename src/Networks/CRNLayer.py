6bf1db8383c6521d557f62291af411c407d00161
matteo6198 <matteo6198@live.it>
2022-01-31 16:13:21 +0100

    use dilated conv instead of downsampling

--- src/Networks/CRNLayer.py
+++ src/Networks/CRNLayer.py
@@ -9,9 +9,9 @@ from Utils.constants import FEATURES_DIM
 class CRNLayer(nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.conv1 = nn.Conv2d(args.features_dim, 32, (3,3), padding=1)
-        self.conv2 = nn.Conv2d(args.features_dim, 32, (5,5), padding=2)
-        self.conv3 = nn.Conv2d(args.features_dim, 20, (7,7), padding=3)
+        self.conv1 = nn.Conv2d(args.features_dim, 32, (3,3), padding=2, dilation=2)
+        self.conv2 = nn.Conv2d(32, 32, (3,3), padding=2, dilation=2)
+        self.conv3 = nn.Conv2d(32, 20, (3,3), padding=2, dilation=2)
 
         self.accumulate = nn.Conv2d(84, 1, (1,1))
 
@@ -30,14 +30,14 @@ class CRNLayer(nn.Module):
     def forward(self, x):
         N, C, W, H = x.shape
         # build contextual reweighting mask
-        m = F.avg_pool2d(x, 2)
+        #m = F.avg_pool2d(x, 2)
 
-        o1 = F.relu(self.conv1(m))
-        o2 = F.relu(self.conv2(m))
-        o3 = F.relu(self.conv3(m))
-        m = torch.cat((o1,o2,o3), 1)
+        o1 = F.relu(self.conv1(x))      # 3x3 filter 
+        o2 = F.relu(self.conv2(o1))     # equivalent to 5x5 filter
+        o3 = F.relu(self.conv3(o2))     # receptive field 7x7 filter
+        m = torch.cat((o1, o2, o3), 1)
         m = self.accumulate(m)
-        m = F.interpolate(m, (W, H), mode='bilinear', align_corners=False)
+        #m = F.interpolate(m, (W, H), mode='bilinear', align_corners=False)
         # residual
         x = F.normalize(x, p=2, dim=1)
         soft_assign = self.conv(x).view(N, self.num_clusters, -1)
